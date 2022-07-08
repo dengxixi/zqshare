fcc899efa3d5807072a714d4f13199145c7d3041
Jons <>
2022-07-08 18:30:20 +0800

    first commit

---
+++ ZQTestMannager.h
@@ -0,0 +1,16 @@
+//
+//  ZQTestMannager.h
+//  zqshare
+//
+//  Created by Jons on 2022/7/8.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface ZQTestMannager : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ ZQTestMannager.m
@@ -0,0 +1,12 @@
+//
+//  ZQTestMannager.m
+//  zqshare
+//
+//  Created by Jons on 2022/7/8.
+//
+
+#import "ZQTestMannager.h"
+
+@implementation ZQTestMannager
+
+@end
